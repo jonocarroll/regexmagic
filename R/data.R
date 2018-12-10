2c9e059339881e3fbf3165899cee7c31e5bbf34f
Bernie Gray <bfgray3@gmail.com>
2018-12-10 12:05:17 -0500

    document identifiers dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Example data to be used for testing.
+#'
+#' This is reproducibly constructed, but should
+#' appear somewhat random, with at least some
+#' strings not matching the given patterns.
+#'
+#' In the inital version of this, the lengths of the strings
+#' will determine an initial split, but this should be
+#' relaxed in further iterations.
+#'
+#' @format A character vector of length 65.
+"identifiers"
